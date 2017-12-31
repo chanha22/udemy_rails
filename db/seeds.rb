b6f2e0020fd5445ebb3f9217f439c34392ccca67
chanha22 <sgu960401@sogang.ac.kr>
2017-12-31 14:03:54 +0900

    implemented custom scopes for portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+
+  )
+end
+
+puts "3개의 topic이 생성되었습니다"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "rails is very nice framework"
+    body: "rails is very nice framework",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,11 +26,22 @@ end
 
 puts "5개의 skill이 생성되었습니다"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio_title:
     #{portfolio_item}",
-    subtitle: "my great service",
+    subtitle: "Ruby on Rails",
+    body: "show me the money, show me the money, show me the money, show me the money, show me the money",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio_title:
+    #{portfolio_item}",
+    subtitle: "Angular",
     body: "show me the money, show me the money, show me the money, show me the money, show me the money",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200"
