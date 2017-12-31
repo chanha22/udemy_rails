eb609d10c905a26bebcc016cc5f99c67131920da
chanha22 <sgu960401@sogang.ac.kr>
2017-12-31 14:50:32 +0900

    Implemented concern for data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171229093539) do
+ActiveRecord::Schema.define(version: 20171231053443) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20171229093539) do
     t.integer  "percent_utilized"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.text     "badge"
   end
 
   create_table "topics", force: :cascade do |t|
