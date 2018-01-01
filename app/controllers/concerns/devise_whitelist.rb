07bb511fc754f35516c44af9fa7116057d26fcc9
chanha22 <sgu960401@sogang.ac.kr>
2018-01-01 20:09:50 +0900

    Complemented authentication with virtual attrs for user model

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -1,11 +1,11 @@
 module DeviseWhitelist
   extend ActiveSupport::Concern
 
-  include do
+  included do
     before_filter :configure_permitted_parameters, if: :devise_controller?
     #devise controller와 관련있을 때만 이를 실행한다
   end
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
