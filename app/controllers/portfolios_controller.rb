2ea6306292896fa0b9baf810de7c1bb392d45adf
chanha22 <sgu960401@sogang.ac.kr>
2018-03-15 14:23:43 +0900

    update portfolio to include attr for position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,7 +3,7 @@ class PortfoliosController < ApplicationController
   access all: [:show, :index, :angular], user: {except: [:destroy, :create, :new, :update]}, message: "You shall not pass", site_admin: :all
 
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
   def angular
